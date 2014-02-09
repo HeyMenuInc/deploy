d8fc3685bc90d52fb354f389e9322e464f50c548
Zhou Li <zhoulijoe@gmail.com>
2014-02-09 04:05:27 -0800

    Add vagrant server initialization script

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
-  config.vm.network "private_network", ip: "192.168.50.2"
-  config.vm.synced_folder "/home/zhou/workspace/hummingbird/emenu_server/target/CloudMenuServer/", "/var/lib/tomcat7/webapps/ROOT/"
+  config.vm.network "public_network"
+  config.vm.provision :shell, :path => "bootstrap.sh"
+  config.vm.synced_folder "../emenu_server/target/CloudMenuServer/", "/var/lib/tomcat7/webapps/ROOT/"
 end
---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
+
+# add repo for java7
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+apt-get update
+
+apt-get install -y emacs
+
+echo "debconf shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
+echo "debconf shared/accepted-oracle-license-v1-1 seen true" | debconf-set-selections
+apt-get install -q -y oracle-java7-installer
+
+mkdir /var/lib/sqlite
+chmod a+w /var/lib/sqlite
+
+apt-get install -y tomcat7
+echo "JAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/default/tomcat7
+service tomcat7 start
