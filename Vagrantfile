812912de5864489d285df77cd4fa80c0898a5587
Zhou Li <zhoulijoe@gmail.com>
2014-02-09 16:26:21 -0800

    Sync resource code dir and create soft link from within VM

--- Vagrantfile
+++ Vagrantfile
@@ -9,5 +9,6 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
   config.vm.network "public_network"
   config.vm.provision :shell, :path => "bootstrap.sh"
-  config.vm.synced_folder "../emenu_server/target/CloudMenuServer/", "/var/lib/tomcat7/webapps/ROOT/"
+  config.vm.synced_folder "../emenu_server/", "/home/vagrant/emenu_server/"
+  #config.vm.synced_folder "../emenu_server/target/CloudMenuServer/", "/var/lib/tomcat7/webapps/ROOT/"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | t
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 apt-get update
 
-apt-get install -y emacs
+#apt-get install -y emacs
 
 echo "debconf shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
 echo "debconf shared/accepted-oracle-license-v1-1 seen true" | debconf-set-selections
@@ -19,3 +19,8 @@ chmod a+w /var/lib/sqlite
 apt-get install -y tomcat7
 echo "JAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/default/tomcat7
 service tomcat7 start
+
+rm -rf /var/lib/tomcat7/webapps/ROOT
+ln -sf /home/vagrant/emenu_server/target/CloudMenuServer /var/lib/tomcat7/webapps/ROOT
+
+service tomcat7 restart
