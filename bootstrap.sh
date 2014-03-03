2bfdb633bf07386a74dc3f9fa35241f0a2e2ac80
Zhou Li <zhoulijoe@gmail.com>
2014-03-02 17:35:28 -0800

    Install and configure apache2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
 
+# install add-apt-repositories tool
+apt-get install python-software-properties
+
 # add repo for java7
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
@@ -18,13 +21,28 @@ apt-get install -y maven
 mkdir /var/lib/sqlite
 chmod a+w /var/lib/sqlite
 
+# Install apache
+apt-get install apache2
+
 apt-get install -y tomcat7
 echo "JAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/default/tomcat7
-sed -i 's/\\\"\$CATALINA_SH\\\" \$\@\"/\"$CATALINA_SH\" jpda $@"/g' /etc/init.d/tomcat7
+sed -i 's/\\\"\$CATALINA_SH\\\" \$\@\"/\"$CATALINA_SH\" jpda $@\"/g' /etc/init.d/tomcat7
 service tomcat7 start
+
+# Install mod_jk
+sudo apt-get install libapache2-mod-jk
+sed -i '{:a;N;$!ba;s/<\!--\n    <Connector port=\"8009\" protocol=\"AJP\/1.3\" redirectPort=\"8443\" \/>\n    -->/<Connector port=\"8009\" protocol=\"AJP\/1\.3\" redirectPort=\"8443\" \/>/g;t a}' /etc/tomcat7/server.xml
+cp /vagrant/apache2/workers.properties /etc/libapache2-mod-jk/
+
+cp /vagrant/apache2/emenu-server /etc/apache2/sites-available/
+rm /etc/apache2/sites-enabled/000-default
+ln -s /etc/apache2/sites-available/emenu-server /etc/apache2/sites-enabled/emenu-server
+
+# Link tomcat site to server build dir
 service tomcat7 stop
 
 rm -rf /var/lib/tomcat7/webapps/ROOT
 ln -sf /home/vagrant/emenu_server/target/CloudMenuServer /var/lib/tomcat7/webapps/ROOT
 
 service tomcat7 start
+service apache2 restart
