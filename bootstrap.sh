de49678df549d193fd6079c333233100ad335cbe
Zhou Li <zhoulijoe@gmail.com>
2014-03-03 20:12:56 -0800

    Add apache proxy for admintool to make API request against backend server

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # install add-apt-repositories tool
-apt-get install python-software-properties
+#apt-get install python-software-properties
 
 # add repo for java7
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
@@ -16,7 +16,11 @@ echo "debconf shared/accepted-oracle-license-v1-1 select true" | debconf-set-sel
 echo "debconf shared/accepted-oracle-license-v1-1 seen true" | debconf-set-selections
 apt-get install -q -y oracle-java7-installer
 
+# Install maven
 apt-get install -y maven
+
+# Install sqlite3
+apt-get install sqlite3
 
 mkdir /var/lib/sqlite
 chmod a+w /var/lib/sqlite
@@ -29,10 +33,18 @@ echo "JAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/default/tomcat7
 sed -i 's/\\\"\$CATALINA_SH\\\" \$\@\"/\"$CATALINA_SH\" jpda $@\"/g' /etc/init.d/tomcat7
 service tomcat7 start
 
+# Enable header mod
+a2enmod headers
+
 # Install mod_jk
-sudo apt-get install libapache2-mod-jk
+apt-get install libapache2-mod-jk
 sed -i '{:a;N;$!ba;s/<\!--\n    <Connector port=\"8009\" protocol=\"AJP\/1.3\" redirectPort=\"8443\" \/>\n    -->/<Connector port=\"8009\" protocol=\"AJP\/1\.3\" redirectPort=\"8443\" \/>/g;t a}' /etc/tomcat7/server.xml
 cp /vagrant/apache2/workers.properties /etc/libapache2-mod-jk/
+
+# Install mod_proxy
+apt-get install libapache2-mod-proxy-html
+a2enmod proxy
+a2enmod proxy_http
 
 # Enable main web app and admin tool site
 rm /etc/apache2/sites-enabled/000-default
